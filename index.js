abab93c3fd7b2cc49aa008071ded333cbf9923e0
Amaan Sodagar <sodagaramaan786@gmail.com>
2026-05-05 11:09:33 +0530

    commit

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-// mongoose
-//   .connect(
-//     "mongodb+srv://sodagaramaan786:HbiVzsmAJNAm4kg4@cluster0.576stzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-//   )
-//   .then(() => console.log("mongodb connected"))
-//   .catch((err) => console.log("mongo error", err));
+mongoose
+  .connect(
+    "mongodb+srv://sodagaramaan786:HbiVzsmAJNAm4kg4@cluster0.576stzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log("mongo error", err));
 
 
 
@@ -26,15 +26,15 @@ app.use(bodyParser.json());
 
 // const mongoose = require("mongoose"); 
 
-mongoose.connect(
-  "mongodb://admin:Admin%402025@89.116.236.84:27017/portfolio?authSource=admin&authMechanism=SCRAM-SHA-256",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Mongo error:", err));
+// mongoose.connect(
+//   "mongodb://admin:Admin%402025@89.116.236.84:27017/portfolio?authSource=admin&authMechanism=SCRAM-SHA-256",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }
+// )
+//   .then(() => console.log("MongoDB connected"))
+//   .catch(err => console.error("Mongo error:", err));
 
 
 
