0b4a74f39779f3fad352f51c53f9298f5e2f5efa
Amaan Sodagar <sodagaramaan786@gmail.com>
2026-05-26 11:18:23 +0530

    COMMIT

--- index.js
+++ index.js
@@ -90,13 +90,14 @@ app.post('/contact', async (req, res) => {
       message,
     });
 
-    // Configure nodemailer
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
+        pass: process.env.EMAIL_PASS
+      }
     });
 
     const mailOptions = {
